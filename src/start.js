82facf2b120680a68e0c0630f446cee458385500
Artem Derevyanko <artemd@leetio.org>
2022-06-13 21:36:47 +0300

    logging running port

--- src/start.js
+++ src/start.js
@@ -1,4 +1,6 @@
 const app = require("./app")
 
 const port = process.env.PORT ? process.env.PORT : 4000
-app.listen(port)
+app.listen(port, "0.0.0.0", () => {
+    console.log(`server is running on: http://0.0.0.0:${port}`);
+})

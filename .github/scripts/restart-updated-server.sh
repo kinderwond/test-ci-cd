fc1eb94d6580b29d1170fcfe2fc1002f7db80de4
Artem Derevyanko <artemd@leetio.org>
2022-06-14 13:03:43 +0300

    github workflow -> "add exporting env"

--- .github/scripts/restart-updated-server.sh
+++ .github/scripts/restart-updated-server.sh
@@ -13,11 +13,3 @@ git pull origin main
 echo "Install app dependencies"
 sudo rm -rf node_modules
 sudo npm install
-
-printenv
-#echo "Build your app"
-#sudo npm run build
-#
-#echo "Run new PM2 action"
-#sudo cp /home/ubuntu/ecosystem.json ecosystem.json
-#sudo pm2 start ecosystem.json

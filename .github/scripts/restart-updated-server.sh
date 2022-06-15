27170f595c56524e9abe1911030703014bd46b52
Ivan Nedilko <i.nedilkko@gmail.com>
2022-06-15 12:12:20 +0300

    syntax fix againg

--- .github/scripts/restart-updated-server.sh
+++ .github/scripts/restart-updated-server.sh
@@ -1,11 +1,8 @@
 echo "Kill all the running PM2 actions"
-#sudo pm2 kill
+sudo pm2 kill
 
-echo "Jump to app folder"
-cd ~/test-ci-cd
-
-echo "Update app from Git"
-#git checkout main
+echo "Stash changes"
+git stash
 
 echo "Pull updates"
 git pull origin main
@@ -16,6 +13,6 @@ echo "Install app dependencies"
 rm -rf node_modules
 npm install
 
-echo "Print current env"
-printenv
+# echo "Print current env"
+# printenv
 
--- src/start.js
+++ src/start.js
@@ -1,6 +1,6 @@
 const app = require("./app")
 
-const port = process.env.PORT ? process.env.PORT : 4000
+const port = process.env.PORT ? process.env.PORT : 8081
 app.listen(port, "0.0.0.0", () => {
     console.log(`server is running on: http://0.0.0.0:${port}`);
 })

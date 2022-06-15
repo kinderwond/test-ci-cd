d18296d18bdee995920c97d4f496b4e3fb2c7b30
Ivan Nedilko <i.nedilkko@gmail.com>
2022-06-15 12:21:30 +0300

    test updates

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const app = new Koa();
 
 app.use(async ctx => {
     console.log("LOG REQUEST")
-    ctx.body = 'Hello ci cd test!';
+    ctx.body = 'CI CD TESTED SUCCESS v1?';
 });
 
 module.exports = app
